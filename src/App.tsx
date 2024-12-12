ba8545ea6079c7714575188889356cfd5b0a48e0
TCDev69 <siforse99@gmail.com>
2024-12-12 21:44:55 +0100

    si

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/Register";
 import Editor from "./pages/Editor";
+import NoteEditor from "./pages/test";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/editor" element={<Editor />} />
+      <Route path="/test" element={<NoteEditor />} />
     </Routes>
   );
 }
